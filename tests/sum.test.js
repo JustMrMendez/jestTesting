406d0fe5025c707fc1250d90dbda0c61138d71b4
Mr. Mendez <mrmendez@amvestor.com>
2022-10-05 23:23:55 +0000

    test 1 done

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,8 +1,7 @@
 // test the function sum from src\app.js
 
-const sum = require("../src/app.js");
+import sum from "../src/app.js";
 
 test("adds 1 + 2 to equal 3", () => {
-    expect(sum(1, 2)).toBe(3);
-}
-);
+    expect(sum(2, 2)).toBe(4);
+});
